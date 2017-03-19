30ebc3d0094923b2de2b03c0e7c23ae46107c49d
Dhruvin Gandhi <dhruvin2910@gmail.com>
2017-03-19 13:18:27 +0530

    Add success to /data

--- endpoints/data.js
+++ endpoints/data.js
@@ -4,7 +4,7 @@ module.exports = connection => (req, res) => {
     const round = results[0].round
     connection.query('SELECT * FROM participations WHERE event = ? ', [event], (_, results) => {
       const participations = results.filter(result => result.status.length == round)
-      res.json({event, round, participations})
+      res.json({success: true, event, round, participations})
     })
   })
 }
