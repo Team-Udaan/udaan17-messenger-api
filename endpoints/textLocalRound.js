cf067cda590ac6f02149bb14e787d37b785d9d24
Dhruvin Gandhi <dhruvin2910@gmail.com>
2017-03-19 18:00:03 +0530

    Handle updated status using split, splice and join

--- endpoints/textLocalRound.js
+++ endpoints/textLocalRound.js
@@ -1,11 +1,13 @@
 module.exports = connection => (req, res) => {
   const {number, status, customID, datetime} = req.body,
     mobile = number.slice(2),
-    event = customID.slice(0, customID.length - 2),
+    event = customID.slice(0, customID.length - 1),
     round = customID[customID.length - 1]
   connection.query('SELECT status FROM participations WHERE mobile = ? AND event = ?', [mobile, event], (_, results) => {
+    let updatedStatus = results[0].status.split('')
+    updatedStatus.splice(round - 1, 1, status)
     connection.query('UPDATE participations SET status = ? WHERE mobile = ? AND event = ?', [
-      results[0].status.split('').splice(round, 1, status), mobile, event
+      updatedStatus.join(''), mobile, event
     ], () => {
       res.end()
     })
